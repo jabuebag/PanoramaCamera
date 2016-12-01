4d856b456c57c6734717dd61866f3ea9243910c2
jabuebag <ybagyang@gmail.com>
2016-12-01 11:34:06 -0800

    add camera library

--- PanoramaCamera/ViewController.swift
+++ PanoramaCamera/ViewController.swift
@@ -7,22 +7,45 @@
 //
 
 import UIKit
+import DKCamera
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var showImage: UIImageView!
     
+    let camera = DKCamera()
+    
     var imageProcessUtil = ImageProcessUtil()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let image = UIImage(named: "haha.jpg")
-        showImage.image = imageProcessUtil.projectImage(image!)
+//        let image = UIImage(named: "haha.jpg")
+//        showImage.image = imageProcessUtil.projectImage(image!)
+        configureCamera()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func startCamera(_ sender: Any) {
+        self.present(camera, animated: true, completion: nil)
+    }
+    
+    func configureCamera() {
+        camera.didCancel = { () in
+            print("didCancel")
+            
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        camera.didFinishCapturingImage = {(image: UIImage) in
+            print(image.size)
+            
+            // self.dismiss(animated: true, completion: nil)
+        }
     }
 
 
---
+++ Pods/Target Support Files/Pods-PanoramaCamera/Pods-PanoramaCamera-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PanoramaCamera : NSObject
+@end
+@implementation PodsDummy_Pods_PanoramaCamera
+@end
