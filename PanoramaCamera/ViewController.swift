d6bf4e6cc2128d7f78c310487dd776ad21a9f583
jabuebag <ybagyang@gmail.com>
2016-12-01 09:59:17 -0800

    first commit

---
+++ PanoramaCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PanoramaCamera
+//
+//  Created by Kris Yang on 2016-12-01.
+//  Copyright © 2016 Kris Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
